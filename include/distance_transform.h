840d2597d826115c31b03b8c7e73c749134ee68a
WenZheng Li <me@wenzheng.li>
2020-02-25 22:11:28 +0900

    seperated distance transform into submodule

--- include/distance_transform.h
+++ include/distance_transform.h
@@ -1,10 +1,1 @@
-#pragma once
-
-// 1D + 2D manhattan distance transforms work in-place by setting dst = src
-void manhattan_distance_transform_1d(int* dst, const int* src, int len, int stride);
-void manhattan_distance_transform_2d(int* dst, const int* src, int x_len, int y_len);
-
-// 1D square distance transform DOES NOT work in-place
-void square_distance_transform_1d(int* dst, const int* src, int len, int stride);
-// 2D version works in-place, also REQUIRES dst buffer to be size (x_len + 1)*(y_len + 1)
-void square_distance_transform_2d(int* dst, const int* src, int x_len, int y_len);
+../submodules/distance_transform/distance_transform.h
